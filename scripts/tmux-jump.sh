cc7f5ebbb95d28f7394e2d41e8fc05b280e54a02
Dzmitry Paulenka <paulenka.dk@gmail.com>
2021-06-06 21:50:51 +0300

    alternative key + chruby support

--- scripts/tmux-jump.sh
+++ scripts/tmux-jump.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+function dot-safe() {
+  local file="$1"
+  if [[ -f "$file" ]]; then
+    source $file
+  fi
+}
+
 tmp_file="$(mktemp)"
 tmux command-prompt -1 -p 'char:' "run-shell \"printf '%1' >> $tmp_file\""
 
@@ -8,4 +15,7 @@ source $current_dir/utils.sh
 export JUMP_BACKGROUND_COLOR=$(get_tmux_option "@jump-bg-color" "\e[0m\e[32m")
 export JUMP_FOREGROUND_COLOR=$(get_tmux_option "@jump-fg-color" "\e[1m\e[31m")
 export JUMP_KEYS_POSITION=$(get_tmux_option "@jump-keys-position" "left")
+
+dot-safe "/usr/local/share/chruby/chruby.sh"
+dot-safe "/usr/local/share/chruby/auto.sh"
 ruby "$current_dir/tmux-jump.rb" "$tmp_file"
